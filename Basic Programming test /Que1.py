c1ff2ace5743e9a5a0d325302fc03e54a593a190
Nithya21sk <karpagakalyani197@gmail.com>
2026-03-31 19:37:34 +0530

    Create Que1.py

---
+++ Basic Programming test /Que1.py
@@ -0,0 +1,27 @@
+score=int(input ("enter a number"))
+income=int(input("enter a number"))
+emi=int(input ("enter a number"))
+status=input ("enter a string"))
+if score>=600 and score<=749:
+  print("review")
+elif score>=749:
+  print("approved")
+else:
+  print("rejected")
+if income <=25000:
+  print("enough")
+else:
+  print("rejected")
+if emi<=50:
+  print ("not exceed")
+else:
+  print("exceed")
+if status==salaried or status==self-employed:
+  print("eligible")
+else:
+  print ("rejected")
+if score>=800
+ print("interest rate:7%")
+elif score>=750 and score<=799:
+ print("intrest rate:8%")
+  
