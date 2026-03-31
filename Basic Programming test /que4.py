f4514eb06007e1b77b97ea7b8c444c2ccf6ebed8
Nithya21sk <karpagakalyani197@gmail.com>
2026-03-31 20:03:16 +0530

    Create que4.py

---
+++ Basic Programming test /que4.py
@@ -0,0 +1,19 @@
+amount=int(input("enter a number"))
+location=input("enter a string"))
+time=int(input("enter a number"))
+failed_attempts=int(input("enter a number"))
+if amount>50000:
+  print(" Risk factor")
+else:
+  print(" done")
+if time>= 00:00 and time<=05:00:
+ print("flagged")
+if location==new:
+  print("flagged")
+if attempts>=3:
+  print (" lock")
+elif attempts >=2:
+  print("High")
+else:
+  print("low")
+  
