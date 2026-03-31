511c256ea8a90bfb4f38f4342a666c7e83dffe1f
Nithya21sk <karpagakalyani197@gmail.com>
2026-03-31 19:47:59 +0530

    Create que3.py

---
+++ Basic Programming test /que3.py
@@ -0,0 +1,5 @@
+total_messages=int(input("enter a number"))
+if total_messages<=100:
+  print("Status:Noraml,Active:100,Compressed:[difference]")
+else:
+  print(" Status: Optimized, Active:100,Compressed:[difference]")
